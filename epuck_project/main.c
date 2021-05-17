56484d8bdbf80f7827b6ed2e14cbd7f5038542e6
Mark1232 <mseward.mark@gmail.com>
2021-05-17 17:58:24 +0100

    Lab 3 done

--- epuck_project/main.c
+++ epuck_project/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 
+
 #include "ch.h"
 #include "hal.h"
 #include "memory_protection.h"
@@ -14,6 +15,8 @@
 #include "sensors/proximity.h"
 #include "chprintf.h"
 #include "usbcfg.h"
+#include "sensors/VL53L0X/VL53L0X.h"
+
 
 
 #define RAND_MAX 500
@@ -36,34 +39,91 @@ int main(void)
     clear_leds();
     spi_comm_start();
     calibrate_ir();
+	VL53L0X_start();
 
-    int r, dir = 1;
+    int flag;
+	int threshold = 75;
 
     /* Infinite loop. */
     while (1) {
-    	for (int i = 0; i<8;i++){
-    		prox_values[i] = get_prox(i);
-    	}
 
-    	if (prox_values[0] > 150 || prox_values[7] > 150) {
+    	flag = 0;
+		// Rotate to find object
+		while((get_calibrated_prox(7) < threshold-20) || (get_calibrated_prox(0) > threshold-20)){
+			// Slowly turn to find object
+			left_motor_set_speed(200);
+			right_motor_set_speed(-200);
+			chThdSleepMilliseconds(100);
+		}
 
-    		if (prox_values[0] < prox_values[7]){
-    		left_motor_set_speed(500);
-    		right_motor_set_speed(-600);
-    		}
+		// Approach object slowly
+		left_motor_set_speed(200);
+		right_motor_set_speed(200);
 
-    		if (prox_values[0] > prox_values[7]){
-    		left_motor_set_speed(-500);
-    		right_motor_set_speed(600);
-    		}
+		// If close to target then stop
+		while(!((get_calibrated_prox(7) > threshold) || (get_calibrated_prox(0) > threshold))){
+		}
 
-    		chThdSleepMilliseconds(500);
-    	}
-    	else {
-    		left_motor_set_speed(600);
-    		right_motor_set_speed(600);
-    	}
-    }
+		left_motor_set_speed(0);
+		right_motor_set_speed(0);
+		set_body_led(1);
+		chThdSleepMilliseconds(1000);
+
+		// Track object
+		while(1){
+			// If close then reverse slowly (maybe add leway before threshold - deadzone)
+			if((get_calibrated_prox(7) > threshold + 10) && (get_calibrated_prox(0) > threshold + 10)){
+				left_motor_set_speed(-500);
+				right_motor_set_speed(-500);
+				chThdSleepMilliseconds(100);
+			}
+/*
+			// If object is lost
+			else if((get_calibrated_prox(7) < 10) && (get_calibrated_prox(0) < 10)){
+				set_body_led(0);
+				break;
+			}
+*/
+			// If further away then approach (maybe add leway after threshold)
+			else if((get_calibrated_prox(7) < threshold - 10) && (get_calibrated_prox(0) < threshold - 10)){
+				left_motor_set_speed(500);
+				right_motor_set_speed(500);
+				chThdSleepMilliseconds(100);
+			}
+
+			// Might have to set these to more smooth turns i.e. no negatives
+			// If object is to the left then turn
+			else if(get_calibrated_prox(7) - 10  > get_calibrated_prox(0)){
+				// Turn left
+				left_motor_set_speed(-300);
+				right_motor_set_speed(300);
+				while(get_calibrated_prox(7) > get_calibrated_prox(0)){
+				}
+				left_motor_set_speed(0);
+				right_motor_set_speed(0);
+			}
+
+			// If object is to the right then turn
+			else if(get_calibrated_prox(0) - 10  > get_calibrated_prox(7)){
+				// Turn Right
+				left_motor_set_speed(300);
+				right_motor_set_speed(-300);
+				while(get_calibrated_prox(0) > get_calibrated_prox(7)){
+				}
+				left_motor_set_speed(0);
+				right_motor_set_speed(0);
+			}
+
+			// Stop
+			else{
+				left_motor_set_speed(0);
+				right_motor_set_speed(0);
+				chThdSleepMilliseconds(300);
+			}
+
+			chThdSleepMilliseconds(150);
+		}
+	}
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
